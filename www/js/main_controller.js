55677da0d4f1bad713f05dd8a39d1c3ad5261682
Matt Keef <mkeef1@gmail.com>
2014-10-15 17:49:28 -0500

    timer working

--- www/js/main_controller.js
+++ www/js/main_controller.js
@@ -2,7 +2,23 @@
   'use strict';
 
   angular.module('mk-timer')
-  .controller('MainCtrl', ['$scope', function($scope){
+  .controller('MainCtrl', ['$scope', '$interval', function($scope, $interval){
+    $scope.time = 100;
+    var timer;
 
-    }]);
+        function countdown(){
+          $scope.time -= 1;
+
+          if($scope.time <= 0){
+            $interval.cancel(timer);
+          }
+        }
+
+        $scope.start = function(){
+          timer = $interval(countdown, 1000);
+          console.log('$scope.time', $scope.time);
+          console.log('$scope.display', $scope.display);
+          console.log('$scope.timer', $scope.timer);
+        };
+  }]);
 })();
