1725687df2035dcbde01018d7846c403ffab1a1a
Matt Keef <mkeef1@gmail.com>
2014-10-15 21:11:41 -0500

    timer stopping on 0

--- www/js/main_controller.js
+++ www/js/main_controller.js
@@ -3,22 +3,21 @@
 
   angular.module('mk-timer')
   .controller('MainCtrl', ['$scope', '$interval', function($scope, $interval){
-    $scope.time = 100;
+    $scope.time = '';
     var timer;
 
-        function countdown(){
-          $scope.time -= 1;
+    function countdown(){
+      $scope.time -= 1;
 
-          if($scope.time <= 0){
-            $interval.cancel(timer);
-          }
-        }
+      if($scope.time <= 0){
+        $interval.cancel(timer);
+      }
+    }
 
-        $scope.start = function(){
-          timer = $interval(countdown, 1000);
-          console.log('$scope.time', $scope.time);
-          console.log('$scope.display', $scope.display);
-          console.log('$scope.timer', $scope.timer);
-        };
+    $scope.start = function(time){
+      time = parseFloat($scope.time);
+      timer = $interval(countdown, 1000);
+      console.log('$scope.time', $scope.time);
+      };
   }]);
 })();
