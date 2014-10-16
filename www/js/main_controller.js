9c67158ed94ef9b04a953e15c03858df89808f5c
Matt Keef <mkeef1@gmail.com>
2014-10-16 00:04:40 -0500

    done

--- www/js/main_controller.js
+++ www/js/main_controller.js
@@ -3,19 +3,24 @@
 
   angular.module('mk-timer')
   .controller('MainCtrl', ['$scope', '$interval', function($scope, $interval){
-    $scope.time = '';
+    $scope.input = '';
     var timer;
 
     function countdown(){
       $scope.time -= 1;
 
       if($scope.time <= 0){
+        navigator.vibrate(3000);
         $interval.cancel(timer);
+        alert('times up!!!');
+        $scope.time = null;
       }
     }
 
     $scope.start = function(time){
-      time = parseFloat($scope.time);
+      time = parseFloat($scope.input);
+      $scope.time = time;
+      $scope.input = '';
       timer = $interval(countdown, 1000);
       console.log('$scope.time', $scope.time);
       };
